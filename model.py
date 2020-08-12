42113e2b47d41ad77b09e220c654fd9b4bd046c7
Stephanie Resis <sresis@umich.edu>
2020-08-12 14:15:13 -0700

    added Playlist and Song Pref classes

--- model.py
+++ model.py
@@ -19,7 +19,35 @@ class User(db.Model):
     lname = db.Column(db.String(50))
     password = db.Column(db.String(50))
 
-    # ratings = a list of Rating objects
-
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
+
+class Playlist(db.Model):
+	"""A playlist."""
+
+	__tablename__ = "playlists"
+	playlist_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+	user_1_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+	user_2_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+	def __repr__(self):
+		return f'<Playlist playlist_id={self.playlist_id}>'
+
+class Song_Pref(db.Model):
+	"""A song preference."""
+
+	__tablename__ = 'song_prefs'
+
+	song_pref_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+	song_title = db.Column(db.String())
+	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+	def __repr__(self):
+		return f'<Song Pref ID song_pref_id={self.song_pref_id} Title song_title={self.song_title}>'
+
+
+
