b408f5060e1301992fcf4ea8c96f1015a3086d78
Stephanie Resis <sresis@umich.edu>
2020-08-18 22:45:14 -0700

    add react routes for home, login, users

--- static/js/root.jsx
+++ static/js/root.jsx
@@ -5,41 +5,61 @@ const Prompt = ReactRouterDOM.Prompt;
 const Switch = ReactRouterDOM.Switch;
 const Redirect = ReactRouterDOM.Redirect;
 
+function App() {
+	return (
+	    <Router>
+	      <div>
+	        <nav>
+	          <ul>
+	            <li>
+	              <Link to="/">Home</Link>
+	            </li>
+	            <li>
+	              <Link to="/login">Login</Link>
+	            </li>
+	            <li>
+	              <Link to="/users">Users</Link>
+	            </li>
+	          </ul>
+	        </nav>
 
+	        {/* A <Switch> looks through its children <Route>s and
+	            renders the first one that matches the current URL. */}
+	        <Switch>
+	          <Route path="/login">
+	            <Login />
+	          </Route>
+	          <Route path="/users">
+	            <Users />
+	          </Route>
+	          <Route path="/">
+	            <Homepage />
+	          </Route>
+	        </Switch>
+	      </div>
+	    </Router>
+  );
+}
 
 function Homepage() {
-	return <div> Welcome to my site </div>
+	return <div> Welcome to my site </div>;
 }
 
+function Users() {
+	return <div>Users**</div>;
+		
 
-function App() {
-	
-	return(
-		<Router>
-			<div>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-
-						</li>
-
-						<li>
-							<Link to="/users">Users</Link>
-
-						</li>
-
-					</ul>
-
-				</nav>
-
-			</div>
-
-		</Router>
-
-
-	)
 }
-
+function Login(props){
+	return (
+	<div>
+		Email:
+		<input type="text"></input>
+		Password:
+		<input type="text"></input>
+		<button>Login</button>
+	</div>
+	);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
