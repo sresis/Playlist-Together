d08883c2459879cb99a72ee29a815163edc4a125
Stephanie Resis <sresis@umich.edu>
2020-08-22 10:37:12 -0700

    hide users except when requested

--- static/ajax.js
+++ static/ajax.js
@@ -6,16 +6,19 @@ $('#view-users').on('click', () => {
 	$.get('/api/users', (response) => {
 
 		// disable button 
-		$("#view-users").attr("disabled", true);
+		$('#view-users').attr('disabled', true);
+
 		
 		// show each user and a link to their profile
 		const info_array = [];
 		for (const item of Object.values(response)) {
 			$('#users-container').append
-			(`<li><a href="/users/${item.user_id}">${item.email}</li></a>`);
+			(`<li><a id='${item.user_id}' href='/users/${item.user_id}'>${item.email}</li></a>`);
 		}
-		
-		
+		// how to make sure that it doesn't keep adding to list if you click
+
+		// show the users list
+		$('#users-container').show();		
 	});
 
 
