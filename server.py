58d4b9740106b13132681bed71eceb6c048bffa8
Stephanie Resis <sresis@umich.edu>
2020-09-22 13:06:50 -0500

    similar users logic

--- server.py
+++ server.py
@@ -265,9 +265,9 @@ def get_similar_user():
 
 	#stores min difference
 	min_diff = [1000, '']
-	# iterate through all users that are not the session user.
+	# iterate through all users that are not the session user that have recs.
 	for user_x in all_users:
-		if user_x != user and crud.user_has_song_recs(user_x) == True:
+		if user_x != user and crud.get_user_song_recs(user_x) is not None:
 			user_diff = (abs(user.user_valence - user_x.user_valence)/session_user_stdev['valence']) 
 			+ (abs(user.user_speechiness - user_x.user_speechiness)/session_user_stdev['speechiness'])
 			+ (abs(user.user_acousticness - user_x.user_acousticness)/session_user_stdev['acousticness'])
