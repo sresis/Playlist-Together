7745d238b2aad7edee3f017dbd365dedfca9e783
Stephanie Resis <sresis@umich.edu>
2020-12-21 12:08:11 -0800

    udpate get_token

--- api.py
+++ api.py
@@ -20,14 +20,7 @@ print(auth_response_data)
 access_token = auth_response_data['access_token']
 def get_token():
 	"""Returns current token."""
-	auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-	})
-	# convert the response to JSON
-	auth_response_data = auth_response.json()
-	print(auth_response_data)
+
 	access_token = auth_response_data['access_token']
 
 	return access_token
